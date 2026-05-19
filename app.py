48d6755c67a42534d40e59b8e6078742df6983d7
Laiba Ansari <laibaansari1106@gmail.com>
2026-05-19 16:07:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,14 @@ import numpy as np
 import os
 
 # Load model
-
 model_path = os.path.join(os.path.dirname(__file__), 'news_predictor_model.sav')
+
+with open(model_path, 'rb') as f:
+    model = pickle.load(f)
+
 vectorizer_path = os.path.join(os.path.dirname(__file__), 'vectorizer.sav')
 
-model = pickle.load(open(model_path, 'rb'))
+# model = pickle.load(open(model_path, 'rb'))
 vectorizer = pickle.load(open(vectorizer_path, 'rb'))
 st.title("Fake news Prediction System")
 
